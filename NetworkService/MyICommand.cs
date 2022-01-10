a3f2344d36c420622a9592a112dd2cdf3764d525
Bogdan Vilimonovic <bogdan.vilimonovic@gmail.com>
2022-01-10 17:29:35 +0100

    Two parameter Command done

--- NetworkService/MyICommand.cs
+++ NetworkService/MyICommand.cs
@@ -148,4 +148,73 @@ namespace NetworkService
         #endregion
     }
 
+    public class MyICommand<T1, T2> : ICommand, ICommandUndo
+    {
+        Action<T1, T2> _TargetExecuteMethod;
+        Func<T1, T2, bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action<T1, T2> executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action<T1, T2> executeMethod, Func<T1, T2, bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+
+            if (_TargetCanExecuteMethod != null)
+            {
+                T1 tparm1 = (T1)parameter;
+                T2 tparm2 = (T2)parameter;
+                return _TargetCanExecuteMethod(tparm1, tparm2);
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod((T1)parameter, (T2)parameter);
+                if (_TargetUnExecuteMethod != null)
+                {
+                    StaticData.myICommands.Push(this);
+                }
+            }
+        }
+
+        Action _TargetUnExecuteMethod;
+        public MyICommand(Action<T1, T2> executeMethod, Action unExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetUnExecuteMethod = unExecuteMethod;
+        }
+
+        public void UnExecute()
+        {
+            if (_TargetUnExecuteMethod != null)
+            {
+                _TargetUnExecuteMethod();
+            }
+        }
+    }
+
 }
--- NetworkService/ViewModel/NetworkDisplayViewModel.cs
+++ NetworkService/ViewModel/NetworkDisplayViewModel.cs
@@ -3,15 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace NetworkService.ViewModel
 {
     public class NetworkDisplayViewModel : BindableBase
     {
+        public MyICommand<object> GetSender { get; set; }
+        public MyICommand<MouseButtonEventArgs> GetMouseButtonEventArgs { get; set; }
+        public MyICommand<DragEventArgs> GetDragEventArgs { get; set; }
 
+        public MyICommand ClickCommand { get; set; }
         public NetworkDisplayViewModel()
         {
-
+            GetSender = new MyICommand<object>(OnGetSender);
+            GetMouseButtonEventArgs = new MyICommand<MouseButtonEventArgs>(OnGetMouseButtonEventArgs);
+            GetDragEventArgs = new MyICommand<DragEventArgs>(OnGetDragEventArgs);
+            ClickCommand = new MyICommand(OnClickCommand);
         }
+
+        private object parameter;
+
+        public object Parameter
+        {
+            get { return parameter; }
+            set { parameter = value; }
+        }
+
+
+        private void OnGetSender(object parameter)
+        {
+            Parameter = parameter;
+        }
+
+        private MouseButtonEventArgs mouseButtonEventArgs;
+
+        public MouseButtonEventArgs MouseButtonEventArgs
+        {
+            get { return mouseButtonEventArgs; }
+            set { mouseButtonEventArgs = value; }
+        }
+
+        private void OnGetMouseButtonEventArgs(MouseButtonEventArgs e)
+        {
+            MouseButtonEventArgs = e;
+        }
+
+        private DragEventArgs dragEventArgs;
+
+        public DragEventArgs DragEventArgs
+        {
+            get { return dragEventArgs; }
+            set { dragEventArgs = value; }
+        }
+
+        private void OnGetDragEventArgs(DragEventArgs e)
+        {
+            DragEventArgs = e;
+        }
+
+        private void OnClickCommand()
+        {
+            int i = 0;
+        }
+
     }
+
 }
